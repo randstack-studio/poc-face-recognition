b8fd2ffbf8f2dbdea639743da93514319cd145a9
hardy <hardyyudha260@gmail.com>
2023-10-31 11:47:10 +0700

    enhance

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -76,7 +76,7 @@ class AuthController {
     if (auth.user) {
       return response.redirect('/menu');
     }
-    return view.render('biometrik-login-2');
+    return view.render('biometrik-login-3');
   }
 
   async biometricLogin({ request, auth, response }) {
--- public/js/mediapipe.js
+++ public/js/mediapipe.js
@@ -3,13 +3,155 @@ const { FaceLandmarker, FilesetResolver, DrawingUtils } = vision;
 const demosSection = document.getElementById("demos");
 const imageBlendShapes = document.getElementById("image-blend-shapes");
 const videoBlendShapes = document.getElementById("video-blend-shapes");
+const faceMatchResult = document.getElementById("face_match_result");
+const faceMatchBox = document.getElementById("face_match_box");
+const milestone = document.getElementById("milestone");
+const resetButton = document.getElementById("reset_button");
+const galleryModal = new bootstrap.Modal(
+  document.getElementById("submittedModal"),
+  {
+    keyboard: false,
+    backdrop: "static",
+  }
+);
+const milestoneTypes = new Array(
+  {
+    id: 1,
+    name: "Eye Blink",
+    landmark: ["eyeBlinkLeft", "eyeBlinkRight"],
+    label: "Kedipkan Mata",
+    threshold: 0.5,
+    status: false,
+    message: "Blink detected",
+  },
+  {
+    id: 2,
+    name: "Eye Squint",
+    landmark: ["eyeSquintLeft", "eyeSquintRight"],
+    label: "Tengok Kanan / Kiri",
+    threshold: 0.5,
+    status: false,
+    message: "Squint detected",
+  },
+  {
+    id: 3,
+    name: "Open Jaw",
+    landmark: ["jawOpen"],
+    label: "Buka Mulut",
+    threshold: 0.5,
+    status: false,
+    message: "Jaw Open detected",
+  },
+  {
+    id: 4,
+    name: "Smile",
+    landmark: ["mouthSmileLeft", "mouthSmileRight"],
+    label: "Senyum",
+    threshold: 0.6,
+    status: false,
+    message: "Smile detected",
+  },
+  {
+    id: 5,
+    name: "Smile",
+    landmark: ["mouthPucker"],
+    label: "Manyunkan Mulut",
+    threshold: 0.7,
+    status: false,
+    message: "Pucker detected",
+  },
+  {
+    id: 6,
+    name: "Brow Raise",
+    landmark: ["browInnerUp"],
+    label: "Naikan Alis",
+    threshold: 0.7,
+    status: false,
+    message: "Pucker detected",
+  }
+);
 
+let milestoneSelected = [];
+let timer = document.getElementById("timer");
+let timerBox = document.getElementById("timer_box");
+let captured = document.getElementById("captured");
+let boxCapture = document.getElementById("box-capture");
+let container = document.getElementById("container");
+let instruction = document.getElementById("instruction");
+let validateLoading = document.getElementById("validate_loading");
+let capturedFrame = null;
+let synced = false;
 let faceLandmarker;
 let runningMode = "VIDEO";
 let enableWebcamButton;
 let webcamRunning;
+let mediaStream = null;
 const videoWidth = 480;
 
+// const socket = io("http://127.0.0.1:8086", {
+//   transports: ["websocket"], // Use WebSocket transport
+//   withCredentials: true,
+// });
+
+const socket = io("lens.staging.endavolabs.com", {
+  transports: ["polling"], // Use WebSocket transport
+  withCredentials: true,
+});
+
+socket.on("biometricLoginResult", (response) => {
+  console.log(response);
+  if (response.success) {
+    email.value = response.result.email;
+    form.submit();
+  } else {
+    validateLoading.classList.add("hidden");
+    faceMatchBox.classList.remove("hidden");
+    milestone.classList.add("hidden");
+    faceMatchResult.innerHTML = response.message;
+    resetButton.classList.remove("hidden");
+    galleryModal.hide();
+  }
+});
+
+function startTimer() {
+  let count = 3;
+  let interval = setInterval(() => {
+    count--;
+    timer.innerHTML = count;
+    if (count == 0) {
+      capture();
+      let ids = [];
+      for (let i = 1; i <= 3; i++) {
+        let id = Math.floor(Math.random() * milestoneTypes.length) + 1;
+        while (ids.includes(id)) {
+          id = Math.floor(Math.random() * milestoneTypes.length) + 1;
+        }
+        ids.push(id);
+      }
+      milestone.innerHTML = `
+      <p class='p-red text-center text-lg' id="mls-${ids[0]}">${
+        milestoneTypes.find((mls) => mls.id == ids[0]).label
+      }</p>
+      <p class='p-red text-center text-lg' id="mls-${ids[1]}">${
+        milestoneTypes.find((mls) => mls.id == ids[1]).label
+      }</p>
+      <p class='p-red text-center text-lg' id="mls-${ids[2]}">${
+        milestoneTypes.find((mls) => mls.id == ids[2]).label
+      }</p>`;
+
+      milestoneSelected = [
+        milestoneTypes.find((mls) => mls.id == ids[0]),
+        milestoneTypes.find((mls) => mls.id == ids[1]),
+        milestoneTypes.find((mls) => mls.id == ids[2]),
+      ];
+      milestone.classList.remove("hidden");
+      clearInterval(interval);
+      timerBox.classList.add("hidden");
+      // startVideo();
+      // capture();
+    }
+  }, 1000);
+}
 async function createFaceLandmarker() {
   const filesetResolver = await FilesetResolver.forVisionTasks(
     "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.3/wasm"
@@ -24,6 +166,7 @@ async function createFaceLandmarker() {
     numFaces: 1,
   });
   demosSection.classList.remove("invisible");
+  enableCam();
 }
 createFaceLandmarker();
 
@@ -102,7 +245,7 @@ async function handleClick(event) {
       { color: "#E0E0E0" }
     );
     drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LIPS, {
-      color: "#E0E0E0"
+      color: "#E0E0E0",
     });
     drawingUtils.drawConnectors(
       landmarks,
@@ -123,9 +266,7 @@ async function handleClick(event) {
 ********************************************************************/
 
 const video = document.getElementById("webcam");
-const canvasElement = document.getElementById(
-  "output_canvas"
-);
+const canvasElement = document.getElementById("output_canvas");
 
 const canvasCtx = canvasElement.getContext("2d");
 
@@ -136,17 +277,17 @@ function hasGetUserMedia() {
 
 // If webcam supported, add event listener to button for when user
 // wants to activate it.
-if (hasGetUserMedia()) {
-  enableWebcamButton = document.getElementById(
-    "webcamButton"
-  );
-  enableWebcamButton.addEventListener("click", enableCam);
-} else {
-  console.warn("getUserMedia() is not supported by your browser");
-}
+// if (hasGetUserMedia()) {
+//   enableWebcamButton = document.getElementById(
+//     "webcamButton"
+//   );
+//   enableWebcamButton.addEventListener("click", enableCam);
+// } else {
+//   console.warn("getUserMedia() is not supported by your browser");
+// }
 
 // Enable the live webcam view and start detection.
-function enableCam(event) {
+function enableCam() {
   if (!faceLandmarker) {
     console.log("Wait! faceLandmarker not loaded yet.");
     return;
@@ -154,20 +295,22 @@ function enableCam(event) {
 
   if (webcamRunning === true) {
     webcamRunning = false;
-    enableWebcamButton.innerText = "ENABLE PREDICTIONS";
+    // enableWebcamButton.innerText = "ENABLE PREDICTIONS";
   } else {
     webcamRunning = true;
-    enableWebcamButton.innerText = "DISABLE PREDICTIONS";
+    // enableWebcamButton.innerText = "DISABLE PREDICTIONS";
   }
 
   // getUsermedia parameters.
   const constraints = {
-    video: true
+    video: true,
   };
 
   // Activate the webcam stream.
   navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
     video.srcObject = stream;
+    mediaStream = stream;
+    startTimer();
     video.addEventListener("loadeddata", predictWebcam);
   });
 }
@@ -249,6 +392,30 @@ async function predictWebcam() {
     window.requestAnimationFrame(predictWebcam);
   }
 }
+async function capture() {
+  // console.log("CAPTURED")
+  const empty = document.getElementById("empty");
+  const track = mediaStream.getVideoTracks()[0];
+  const imageCapture = new ImageCapture(track);
+
+  capturedFrame = await imageCapture.takePhoto();
+  captured.src = URL.createObjectURL(capturedFrame);
+}
+
+async function sync() {
+  boxCapture.classList.remove("hidden");
+  container.classList.add("hidden");
+  instruction.classList.add("hidden");
+  galleryModal.show();
+  validateLoading.classList.remove("hidden");
+  const blob = capturedFrame.slice(0, capturedFrame.size, "image/png"); // Adjust the format if needed
+  const capturedFile = new File([blob], "captured_image.png", {
+    type: "image/png",
+  });
+  const currentEmail = localStorage.getItem("email");
+  socket.emit('biometricLogin', {capturedFile: capturedFile, email: currentEmail });
+  // sendSocketLogin(capturedFile);
+}
 
 function drawBlendShapes(el, blendShapes) {
   if (!blendShapes.length) {
@@ -259,9 +426,23 @@ function drawBlendShapes(el, blendShapes) {
 
   let htmlMaker = "";
   blendShapes[0].categories.map((shape) => {
-    if(shape.categoryName == "eyeBlinkLeft" || shape.categoryName == "eyeBlinkRight"){
-      if(shape.score > 0.5){
-        console.log("Blink detected");
+    // console.log(shape);
+    milestoneSelected.map((mls) => {
+      if (mls.landmark.includes(shape.categoryName)) {
+        if (shape.score > mls.threshold) {
+          mls.status = true;
+          let el = document.getElementById(`mls-${mls.id}`);
+          el.classList.remove("p-red");
+          el.classList.add("p-green");
+        }
+      }
+    });
+    if (milestoneSelected.length > 0) {
+      if (milestoneSelected.every((mls) => mls.status == true)) {
+        if (synced == false) {
+          sync();
+          synced = true;
+        }
       }
     }
     htmlMaker += `
